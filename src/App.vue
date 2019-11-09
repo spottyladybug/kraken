67d5b82332dfbc635288d9b168b8f9fa8aaafbdc
vladdlab <vladislavbochmanov@gmail.com>
2019-11-09 14:46:32 +0300

    sdfd# Please enter the commit message for your changes. Lines starting

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.j App"/>
+    <HelloWorld msg="Welco App"/>
   </div>
 </template>
 
