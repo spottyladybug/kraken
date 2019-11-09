59016d79c183149f502236be113afc6b28a80202
anna <lyalinanna@gmail.com>
2019-11-10 02:21:47 +0300

    remove profile

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,16 +1,11 @@
 <template>
   <div id="home">
       This is Home
-      <Profile/>
   </div>
 </template>
 
 <script>
-import Profile from 'src/components/Profile';
 export default {
-  name: 'Home',
-  components: {
-      Profile
-  }
+  name: 'Home'
 }
 </script>
