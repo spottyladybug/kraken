7f83b8308f9559c2004d2e075da8d5b968ecb534
anna <lyalinanna@gmail.com>
2019-11-10 01:27:29 +0300

    add card

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,11 +1,16 @@
 <template>
   <div id="home">
       This is Home
+      <Profile/>
   </div>
 </template>
 
 <script>
+import Profile from 'src/components/Profile';
 export default {
   name: 'Home',
+  components: {
+      Profile
+  }
 }
 </script>
