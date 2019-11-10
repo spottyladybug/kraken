1ca151e092304e1f3fda3c6126a62b1416935b6d
anna <lyalinanna@gmail.com>
2019-11-10 04:08:19 +0300

    fix

--- src/api/repository.js
+++ src/api/repository.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const Repository = axios.create({
     baseURL: 'http://188.93.211.71:8000', // ввести url сервера
     headers: {
-        'X-Auth-Token': '', //TODO: было бы неплохо
-        'Content-Type': 'application/x-www-form-urlencoded',
+        'Content-Type': 'application/json',
     }
 })
